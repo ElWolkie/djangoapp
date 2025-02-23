d533ebc9b153a8421ad030741e60e42b08a44265
Stelaringt <jireh2003molina@gmail.com>
2025-02-23 03:09:54 -0400

    vistas no funcionales de ofertas, tipos de ofertas, y adicion de cuota sin vista

--- apps/home/urls.py
+++ apps/home/urls.py
@@ -5,11 +5,16 @@ urlpatterns = [
 
     # The home page
     path('', views.index, name='home'),
+    # Matches any html file
+    re_path(r'^.*\.*', views.pages, name='pages'),
+
 
     # Specific route for tipoPersona modal
     path('tipoPersonaModal/', views.tipo_persona_modal, name='tipo_persona_modal'),
+    # Specific route for tipoPersona modal
+    path('tipoOfertaModal/', views.tipo_oferta_modal, name='tipo_oferta_modal'),
+    # Specific route for tipoPersona modal
+    path('ofertaModal/', views.oferta_modal, name='oferta_modal'),
 
-    # Matches any html file
-    re_path(r'^.*\.*', views.pages, name='pages'),
 
 ]
