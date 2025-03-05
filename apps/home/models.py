5a79e5b81ec11dcf84a5d773420e43cc844629e5
Stelaringt <jireh2003molina@gmail.com>
2025-03-05 14:17:56 -0400

    honorario.html culminado, registra correctamente y se muestra las consulta adecuadamente, se eliminola mini tabla dinamica con problemas del modal, y se reemplazo por una busqueda de coincidencias de datos ingresasos apoyado de una alerta con los datos de la persona encontrada para confirmacion de que sea la que se busca, en contrato.html se agrego la misma alerta para estandarizar el comportamiento

--- apps/home/models.py
+++ apps/home/models.py
@@ -114,8 +114,8 @@ class Honorario(models.Model):
     idHonorario = models.AutoField(primary_key=True)  # Clave primaria para el modelo  
     idContrato = models.ForeignKey(Contrato, on_delete=models.CASCADE)  # Llave foránea hacia Contrato
     horas = models.FloatField()  # Cantidad de horas trabajadas  
-    estadoContrato = models.CharField(max_length=10)  # Estado del honorario  
-    fechaContrato = models.DateField(auto_now_add=True)  # Fecha de creación del honorario  
+    estadoHonorario = models.CharField(max_length=10)  # Estado del honorario  
+    fechaHonorario = models.DateField(auto_now_add=True)  # Fecha de creación del honorario  
 
     class Meta:  
         verbose_name = "Honorario"  
