d2a1cfdaf6259502d1a1102dba9629e2c99fec7f
Stelaringt <jireh2003molina@gmail.com>
2025-04-19 00:03:49 -0400

    solucionado bug en materia, honorario, materia al registrar desde los modales

--- apps/persona/signals.py
+++ apps/persona/signals.py
@@ -7,4 +7,4 @@ def create_default_tipo_persona(sender, **kwargs):
     if sender.name == "apps.persona":  # Asegúrate de que coincida con el nombre de la app
         default_tipos = ["Usuario", "Cliente", "Proveedor"]
         for tipo in default_tipos:
-            TipoPersona.objects.get_or_create(nombreTP=tipo, defaults={"estadoTP": "Activo"})
+            TipoPersona.objects.get_or_create(nombreTP=tipo, defaults={"estadoTP": "ACTIVO"})
