9670250c423930249937155cc45dc187e2283d47
unknown <wilexamontillaevies@gmail.com>
2025-04-26 20:50:12 -0400

    html arreglados con acentos y titulos

--- core/settings.py
+++ core/settings.py
@@ -84,7 +84,7 @@ DATABASES = {
         "ENGINE": "django.db.backends.postgresql",
         "NAME": config("DB_NAME", default="fundacion"),
         "USER": config("DB_USER", default="postgres"),
-        "PASSWORD": config("DB_PASSWORD", default="rafaeljesus"),
+        "PASSWORD": config("DB_PASSWORD", default="wil123"),
         "HOST": config("DB_HOST", default="localhost"),
         "PORT": config("DB_PORT", default="5432"),
         "OPTIONS": {
