a54b418dddc5a2b11e6f86811f864994511836f0
unknown <wilexamontillaevies@gmail.com>
2025-06-07 21:48:53 -0400

    wilexa

--- core/settings.py
+++ core/settings.py
@@ -117,9 +117,9 @@ MEDIA_URL = '/media/'
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.postgresql",
-        "NAME": config("DB_NAME", default="fundacion"),
-        "USER": config("DB_USER", default="prueba"),
-        "PASSWORD": config("DB_PASSWORD", default="Prueba2022"),
+        "NAME": config("DB_NAME", default="fundacion2"),
+        "USER": config("DB_USER", default="postgres"),
+        "PASSWORD": config("DB_PASSWORD", default="wil123"),
         "HOST": config("DB_HOST", default="localhost"),
         "PORT": config("DB_PORT", default="5432"),
         'OPTIONS': {
