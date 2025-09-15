b8138c363cc401ea2667a328dda1d5e71813ab58
Stelaringt <jireh2003molina@gmail.com>
2025-09-15 01:30:28 -0400

    Balance de cuentas visible, libro diario mejorado (paginado, calculos, busqueda ahora en bakend, se elimino los 0.00 de los debe y haber vacios)

--- apps/librosContables/views.py
+++ apps/librosContables/views.py
@@ -2,14 +2,52 @@ from django.shortcuts import render
 from django.db.models import Sum
 from apps.asientoContable.models import AsientoContable, DetalleAsiento
 from apps.planCuenta.models import PlanCuenta
-
+from django.core.paginator import Paginator
+from django.db.models import Q
 def libro_diario(request):
     """
     Vista para generar el Libro Diario.
     Muestra los asientos contables y sus detalles en orden cronológico.
     """
+    search_query = request.GET.get('search', '').strip()  # Obtener el término de búsqueda
+    start_date = request.GET.get('start_date')  # Obtener la fecha de inicio
+    end_date = request.GET.get('end_date')  # Obtener la fecha de fin
+
     asientos = AsientoContable.objects.prefetch_related('detalles').order_by('fechaAsiento', 'numeroAsiento')
-    return render(request, 'librosContables/libroDiario.html', {'asientos': asientos})
+
+    # Filtrar por el término de búsqueda si existe
+    if search_query:
+        asientos = asientos.filter(
+            Q(numeroAsiento__icontains=search_query) |
+            Q(fechaAsiento__icontains=search_query) |
+            Q(conceptoAsiento__icontains=search_query) |
+            Q(detalles__idPlanCuenta__nombrePlanCuenta__icontains=search_query) |
+            Q(detalles__idPlanCuenta__codigoPlanCuenta__icontains=search_query) |
+            Q(detalles__debe__icontains=search_query) |
+            Q(detalles__haber__icontains=search_query)
+        ).distinct()
+
+    # Filtrar por rango de fechas si existen
+    if start_date:
+        asientos = asientos.filter(fechaAsiento__gte=start_date)
+    if end_date:
+        asientos = asientos.filter(fechaAsiento__lte=end_date)
+
+    # Calcular los totales de debe y haber
+    totales = asientos.aggregate(
+        total_debe=Sum('detalles__debe'),
+        total_haber=Sum('detalles__haber')
+    )
+
+    paginator = Paginator(asientos, 10)  # 10 asientos por página
+    page_number = request.GET.get('page')
+    page_obj = paginator.get_page(page_number)
+
+    return render(request, 'librosContables/libroDiario.html', {
+        'asientos': page_obj,
+        'total_debe': totales['total_debe'] or 0,
+        'total_haber': totales['total_haber'] or 0
+    })
 
 
 def libro_mayor(request):
