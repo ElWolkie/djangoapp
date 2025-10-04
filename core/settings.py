2f71a85fe8c37882e3c4a89ba6f72a65e68f2db3
Edgar <pedorvlbconyas@gmail.com>
2025-10-04 16:29:57 -0400

    Mas cambios

--- core/settings.py
+++ core/settings.py
@@ -117,7 +117,7 @@ MIDDLEWARE = [
     "whitenoise.middleware.WhiteNoiseMiddleware",
     "corsheaders.middleware.CorsMiddleware",
     "django.middleware.common.CommonMiddleware",
-    "django.middleware.csrf.CsrfViewMiddleware",
+    # "django.middleware.csrf.CsrfViewMiddleware",
     "django.contrib.sessions.middleware.SessionMiddleware",
     "django.contrib.auth.middleware.AuthenticationMiddleware",
     "django.contrib.messages.middleware.MessageMiddleware",
