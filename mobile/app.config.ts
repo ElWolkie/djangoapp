2b0e897277109a97d12eeebf62b614a786957b64
Edgar <pedorvlbconyas@gmail.com>
2025-10-07 23:17:40 -0400

    cambios para hacer funcionar persona e inscripcion de nuevo

--- mobile/app.config.ts
+++ mobile/app.config.ts
@@ -1,10 +1,11 @@
+// app.config.js
 export default {
   expo: {
     name: "MiAppMovil",
     slug: "MiAppMovil",
     version: "1.0.0",
     orientation: "portrait",
-    icon: "./assets/iconApp.png",
+    icon: "./assets/icon.png",
     userInterfaceStyle: "light",
     newArchEnabled: true,
     splash: {
@@ -28,26 +29,11 @@ export default {
       ]
     },
     web: {
-      favicon: "./assets/iconApp.png"
+      favicon: "./assets/favicon.png"
     },
-
-    // <- Aquí actualizamos extra para incluir eas.projectId (ya lo tenías)
     extra: {
-      eas: {
-        projectId: "7f5ca19f-9334-45c0-bad4-4e489757f4e4"
-      },
       API_BASE_URL: process.env.EXPO_PUBLIC_API_BASE_URL ?? "https://djangoapp-6wxv.onrender.com"
     },
-
-    // Añade esto: configuración requerida para EAS Update
-    updates: {
-      url: "https://u.expo.dev/7f5ca19f-9334-45c0-bad4-4e489757f4e4"
-    },
-
-    // Usa runtimeVersion por política de versión de app (recomendado)
-    // Esto permite que EAS Update identifique compatibilidad por app version
-    runtimeVersion: "1.0.0",
-
     plugins: [
       [
         "expo-build-properties",
