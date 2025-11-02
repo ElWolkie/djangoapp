5fa84531367a7caa0d4c75ce164198a75c2dff50
Stelaringt <jireh2003molina@gmail.com>
2025-11-02 14:15:41 -0400

    Correccion paginado de bitacora

--- apps/bitacora/views.py
+++ apps/bitacora/views.py
@@ -55,7 +55,7 @@ def ejecutar_limpieza_bitacora(request):
 class BitacoraListView(ListView):
     model = Bitacora
     template_name = 'bitacora_list.html'
-    paginate_by = 25
+    paginate_by = 1
     context_object_name = 'registros'
 
     def get_queryset(self):
