c3ce80801c8da3ac2c396c0902869a4c39f4597b
Edgar <pedorvlbconyas@gmail.com>
2025-11-02 18:51:27 -0400

    see you

--- apps/api/urls.py
+++ apps/api/urls.py
@@ -21,7 +21,6 @@ urlpatterns = [
     path('notas/usuario/autenticado/', views.NotasUsuarioAutenticadoView.as_view(), name='notas-usuario-autenticado'),
 
     path('pagos/', views.PagoUsuarioListView.as_view(), name='pagos-list-usuario'),
-    path('notas/', views.NotasUsuarioAutenticadoView.as_view(), name='notas-list-usuario'),
 
     path('corregir-relaciones/', corregir_relaciones_notas, name='corregir_relaciones'),
     path('notas/por-cedula/', notas_por_cedula, name='notas_por_cedula'),
