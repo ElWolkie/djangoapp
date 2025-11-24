6dfd8a87450f08cfe29f7876c346ebc8acf1c302
Stelaringt <jireh2003molina@gmail.com>
2025-11-23 22:11:26 -0400

    Correcion de configuracion para que los campos de la app no sean obligatorios

--- apps/home/forms.py
+++ apps/home/forms.py
@@ -311,7 +311,8 @@ class ConfiguracionForm(forms.ModelForm):
             'class': 'form-control text-dark',
             'placeholder': 'Seleccione una cuenta bancaria'
         }),
-        empty_label="Seleccione una cuenta bancaria..."
+        empty_label="Seleccione una cuenta bancaria...",
+        required=False
     )
 
     cedulaCuenta = forms.CharField(
@@ -319,7 +320,8 @@ class ConfiguracionForm(forms.ModelForm):
             'class': 'form-control text-dark', 
             'placeholder': 'Ej: V-12345678', 
             'maxlength': 20
-        })
+        }),
+        required=False
     )
     descuento = forms.DecimalField(
         max_digits=5, 
